a148e95eae0fb0165ec6b216bc6030b2f77a80fd
jizhongce <jizhongce123@gmail.com>
2018-11-29 15:34:58 -0500

    finish adding function to compute ptc*pc

--- Correction/correction.py
+++ Correction/correction.py
@@ -20,7 +20,7 @@ Possible_Candidates = Find_Possible_Candidates(typo)
 
 # This function will compute its p(t|c) for each candidates input
 
-def Find_Candidates_P_T_C(Candidates):
+def Find_Candidates_P_T_C_P_C(Candidates):
     Candidates_Posibility = list()
 
     for Candidate in Candidates:
@@ -38,7 +38,7 @@ def Find_Candidates_P_T_C(Candidates):
                     t_p = letter
                     add_cpMinus1_tp = Insertion_Confusion[getLetterIndex(c_p_minus_1)][getLetterIndex(t_p)]
                     chars_cpMinus1 = charsX[getLetterIndex(c_p_minus_1)]
-                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : add_cpMinus1_tp/chars_cpMinus1})
+                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : add_cpMinus1_tp/chars_cpMinus1, "p_c" : Compute_Pr_c(correct), "p_t_c_p_c" : Compute_Pr_c(correct)*add_cpMinus1_tp/chars_cpMinus1})
 
             elif Candidate == "Deletion":
                 for candidate in Candidate_List:
@@ -51,7 +51,7 @@ def Find_Candidates_P_T_C(Candidates):
                     c_p = correct[position]
                     del_cpMinus1_cp = Deletion_Confusion[getLetterIndex(c_p_minus_1)][getLetterIndex(c_p)]
                     chars_cpMinus1_cp = charsXY[getLetterIndex(c_p_minus_1)][getLetterIndex(c_p)]
-                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : del_cpMinus1_cp/chars_cpMinus1_cp})
+                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : del_cpMinus1_cp/chars_cpMinus1_cp, "p_c" : Compute_Pr_c(correct), "p_t_c_p_c" : Compute_Pr_c(correct)*add_cpMinus1_tp/chars_cpMinus1})
 
             elif Candidate == "Reverse":
                 for candidate in Candidate_List:
@@ -60,7 +60,7 @@ def Find_Candidates_P_T_C(Candidates):
                     c_pos = correct[pos]
                     rev_c_pre_c_pos = Reversal_Confusion[getLetterIndex(c_pre)][getLetterIndex(c_pos)]
                     chars_c_pre_c_pos = charsXY[getLetterIndex(c_pre)][getLetterIndex(c_pos)]
-                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : rev_c_pre_c_pos/chars_c_pre_c_pos})
+                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : rev_c_pre_c_pos/chars_c_pre_c_pos, "p_c" : Compute_Pr_c(correct), "p_t_c_p_c" : Compute_Pr_c(correct)*add_cpMinus1_tp/chars_cpMinus1})
 
             elif Candidate == "Substitution":
                 for candidate in Candidate_List:
@@ -70,14 +70,13 @@ def Find_Candidates_P_T_C(Candidates):
                     t_p = letter
                     sub_tp_cp = Insertion_Confusion[getLetterIndex(t_p)][getLetterIndex(c_p)]
                     chars_cp = charsX[getLetterIndex(c_p)]
-                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : sub_tp_cp/chars_cp})
+                    Candidates_Posibility.append({"Cor": correct, "p_t_c" : sub_tp_cp/chars_cp, "p_c" : Compute_Pr_c(correct), "p_t_c_p_c" : Compute_Pr_c(correct)*add_cpMinus1_tp/chars_cpMinus1})
 
     return(Candidates_Posibility)
 
 
 
-
-print(Find_Candidates_P_T_C(Possible_Candidates))
+print(Find_Candidates_P_T_C_P_C(Possible_Candidates))
 
 
 # print(find_insertion_letters("creased", "ncreased"))
